a75fcd08ad0d1058b13e06fd028f7d8f90625c51
AnandRishu <rishu.ac@yahoo.com>
2021-04-05 08:23:42 +0530

    Assignment7 - Height changes added for show-data class

--- Assignment7/temp.js
+++
@@ -1,7 +0,0 @@
-// var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-var today  = new Date();
-
-console.log(today.toLocaleDateString("en-US")); // 9/17/2016
-console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
-console.log(today.toLocaleDateString("hi-IN", options)); // शनिवार, 17 सितंबर 2016
